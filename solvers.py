ce0842b051cd85bb5d22c48e24ca003481975609
Bálint Aradi <aradi@uni-bremen.de>
2018-05-21 20:07:28 +0200

    Implement partial pivoting
    Note: One test still fails as linear dependency check has not been
    implemented yet.

--- solvers.py
+++ solvers.py
@@ -15,6 +15,7 @@ def gaussian_eliminate(aa, bb):
     """
     nn = aa.shape[0]
     for ii in range(nn - 1):
+        _make_partial_pivot(aa[ii:, :], bb[ii:], ii)
         for jj in range(ii + 1, nn):
             coeff = -aa[jj, ii] / aa[ii, ii]
             aa[jj, ii:] += coeff * aa[ii, ii:]
@@ -24,3 +25,26 @@ def gaussian_eliminate(aa, bb):
     for ii in range(nn - 1, -1, -1):
         xx[ii] = (bb[ii] - np.dot(aa[ii, ii + 1:], xx[ii + 1:])) / aa[ii, ii]
     return xx
+
+
+def _make_partial_pivot(aa, bb, icol):
+    """Makes pivot changes in matrix A and vector b based on a given column of
+    the matrix A.
+
+    It looks for the element with the biggest absolute number in the given
+    columnt of A and exchanges the rows of A, so that the biggest number
+    is in the zeroth row.
+
+    Args:
+        aa: Matirx A on entry, pivoted matrix on exit. Shape (m, n)
+        bb: Vector b on entry, pivoted vector on exit. Shape: (m,)
+        icol: Column number to investigate for the pivoting (0 <= icol < m)
+    """
+    imax = np.argmax(abs(aa[:, icol]))
+    if imax:
+        tmp = np.array(aa[0])
+        aa[0] = aa[imax]
+        aa[imax] = tmp
+        tmp = np.array(bb[0])
+        bb[0] = bb[imax]
+        bb[imax] = tmp
