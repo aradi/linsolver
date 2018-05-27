19e653f704883abcc1fe36a7bb707c6af4e7bed6
Bálint Aradi <aradi@uni-bremen.de>
2018-05-27 12:34:16 +0200

    Use pytest for unit testing

--- test_solvers.py
+++ test_solvers.py
@@ -4,36 +4,32 @@
 import numpy as np
 import solvers
 
-def main():
-    """Main testing function."""
 
-    print("\nTest 1")
+TOLERANCE = 1e-10
+
+
+def test_elimination():
+    "Tests simple elimination"
     aa = np.array([[2.0, 4.0, 4.0], [5.0, 4.0, 2.0], [1.0, 2.0, -1.0]])
     bb = np.array([1.0, 4.0, 2.0])
     xx_expected = np.array([0.666666666666667, 0.416666666666667, -0.5])
     xx_gauss = solvers.gaussian_eliminate(aa, bb)
-    _check_result(xx_expected, xx_gauss)
+    assert np.all(np.abs(xx_gauss - xx_expected) < TOLERANCE)
 
-    print("\nTest 2")
+
+def test_elimination_needing_pivot():
+    "Tests elimination where pivot is needed"
     aa = np.array([[2.0, 4.0, 4.0], [1.0, 2.0, -1.0], [5.0, 4.0, 2.0]])
     bb = np.array([1.0, 2.0, 4.0])
     xx_expected = np.array([0.666666666666667, 0.416666666666667, -0.5])
     xx_gauss = solvers.gaussian_eliminate(aa, bb)
-    _check_result(xx_expected, xx_gauss)
+    assert np.all(np.abs(xx_gauss - xx_expected) < TOLERANCE)
 
-    print("\nTest 3")
+
+def test_elimination_linear_dep():
+    "Tests detection of linear dependency"
     aa = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]])
     bb = np.array([1.0, 2.0, 3.0])
-    xx_expected = None
     xx_gauss = solvers.gaussian_eliminate(aa, bb)
-    _check_result(xx_expected, xx_gauss)
+    assert xx_gauss is None
 
-
-def _check_result(expected, obtained):
-    """Checks results by printing expected and obtained one."""
-    print("Expected:", expected)
-    print("Obtained:", obtained)
-
-
-if __name__ == '__main__':
-    main()
