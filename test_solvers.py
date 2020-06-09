Aski2102/Linex

Make parameterized tests use core io routines

test_solvers.py
@@ -5,45 +5,36 @@ import os.path
 import pytest
 import numpy as np
 import solvers
+import linsolveio as io
 
 
 TOLERANCE = 1e-10
 TESTDATADIR = 'testdata'
 
-TESTS_SUCCESSFUL = ['simple', 'needs_pivot']
-
-TESTS_LINEARDEP = ['linearly_dependant']
+TESTS_SUCCESSFUL = ['simple', 'needs_pivot', 'linearly_dependant']
 
 
-def get_test_input(testname):
+def _get_test_input(testname):
     "Reads the input for a given test."
-    testinfile = os.path.join(TESTDATADIR, testname + '.in')
-    data = np.loadtxt(testinfile)
-    nn = data.shape[1]
-    aa = data[:nn, :]
-    bb = data[nn, :]
+    inputfile = os.path.join(TESTDATADIR, testname, 'linsolve.in')
+    aa, bb = io.read_input(inputfile)
     return aa, bb
 
 
-def get_test_output(testname):
-    "Reads the ouput for a given test."
-    testoutfile = os.path.join(TESTDATADIR, testname + '.out')
-    data = np.loadtxt(testoutfile)
-    return data
+def _get_test_output(testname):
+    "Reads the reference ouput for a given test."
+    testoutfile = os.path.join(TESTDATADIR, testname, 'linsolve.out.ref')
+    result = io.read_result(testoutfile)
+    return result
 
 
 @pytest.mark.parametrize("testname", TESTS_SUCCESSFUL)
-def test_successful_elimination(testname):
+def test_elimination(testname):
     "Tests successful elimination."
-    aa, bb = get_test_input(testname)
-    xx_expected = get_test_output(testname)
+    aa, bb = _get_test_input(testname)
+    xx_expected = _get_test_output(testname)
     xx_gauss = solvers.gaussian_eliminate(aa, bb)
-    assert np.all(np.abs(xx_gauss - xx_expected) < TOLERANCE)
-
-
-@pytest.mark.parametrize("testname", TESTS_LINEARDEP)
-def test_linear_dependancy(testname):
-    "Tests linear dependancy"
-    aa, bb = get_test_input(testname)
-    xx_gauss = solvers.gaussian_eliminate(aa, bb)
-    assert xx_gauss is None
+    if xx_expected is None:
+        assert xx_gauss is None
+    else:
+        assert np.all(np.abs(xx_gauss - xx_expected) < TOLERANCE)

aradi/linsolver

test_solvers.py
@@ -5,6 +5,7 @@ import os.path
 import pytest
 import numpy as np
 import solvers
+import linsolveio as io
 
 
 ABSOLUTE_TOLERANCE = 1e-10
@@ -20,18 +21,15 @@ TESTS_LINEARDEP = ['linearly_dependant']
 def get_test_input(testname):
     "Reads the input for a given test."
     testinfile = os.path.join(TESTDATADIR, testname + '.in')
-    data = np.loadtxt(testinfile)
-    nn = data.shape[1]
-    aa = data[:nn, :]
-    bb = data[nn, :]
+    aa, bb = io.read_input(testinfile)
     return aa, bb
 
 
 def get_test_output(testname):
-    "Reads the ouput for a given test."
+    "Reads the reference ouput for a given test."
     testoutfile = os.path.join(TESTDATADIR, testname + '.out')
-    data = np.loadtxt(testoutfile)
-    return data
+    result = io.read_result(testoutfile)
+    return result
 
 
 @pytest.mark.parametrize("testname", TESTS_SUCCESSFUL)